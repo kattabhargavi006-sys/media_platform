ccaf8599e17a19c95a330bab103f6f831db2151e
kattabhargavi006-sys <kattabhargavi006@gmail.com>
2026-05-21 10:29:56 +0530

    my sql connector replacedd.

--- db_c.py
+++ db_c.py
@@ -1,15 +1,15 @@
-import mysql.connector
-import streamlit  as st
+ import mysql.connector
+import streamlit as st
 
-conn_obj=mysql.connector.connect(
-    host = st.secrets["host"],
+conn_obj = mysql.connector.connect(
+    host=st.secrets["host"],
     database=st.secrets["database"],
     port=st.secrets["port"],
     user=st.secrets["user"],
     password=st.secrets["password"]
 )
 
-cursor_obj=conn_obj.cursor(dictionary=True)
+cursor_obj = conn_obj.cursor(dictionary=True)
 
 # USERS TABLE
 cursor_obj.execute("""
