dab66a24e5ae3419fbba468e987766f34d24d537
kattabhargavi006-sys <kattabhargavi006@gmail.com>
2026-05-21 11:04:47 +0530

    dashboard setup

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from db_c import conn_obj,cursor_obj
+from db_c import conn_obj, cursor_obj
 import cloudinary
 import cloudinary.uploader
+
 st.title("Media Platform")
 
 cloudinary.config(
@@ -10,37 +11,37 @@ cloudinary.config(
     api_secret=st.secrets["api_secret"]
 )
 
-
 if "user" not in st.session_state:
     st.session_state.user = None
 
 def dashboard():
     st.sidebar.success("welcome user")
-    opt=st.sidebar.selectbox("choose :-- ",["uploadFiles","viewFiles","Logout"])
-    st.header("dashboard")  
+    opt = st.sidebar.selectbox("choose :-- ", ["uploadFiles", "viewFiles", "Logout"])
+    st.header("dashboard")
 
     if opt == "uploadFiles":
         st.header("upload yr files here")
-        choosedFile=st.file_uploader("choose file",type=["pdf","jpg","jpeg","png","mp3","mp4"]) 
+        choosedFile = st.file_uploader("choose file", type=["pdf", "jpg", "jpeg", "png", "mp3", "mp4"])
 
         if choosedFile:
             st.write(choosedFile.name)
             st.write(choosedFile.type)
 
-        if "image" in choosedFile.type:
-            st.image(choosedFile)
-        elif "video" in choosedFile.type:
-            st.video(choosedFile)
-        elif "audio" in choosedFile.type:
-            st.audio(choosedFile)  
+            if "image" in choosedFile.type:
+                st.image(choosedFile)
+            elif "video" in choosedFile.type:
+                st.video(choosedFile)
+            elif "audio" in choosedFile.type:
+                st.audio(choosedFile)
 
-        if st.button("upload file to cloudinary"):
-            uploaded_dict_obj=cloudinary.uploader.upload(choosedFile,resource_type="auto") 
-            url=uploaded_dict_obj["secure_url"]             
-            st.write(url)
-            st.write("file uploaded to cloudinary")
+            if st.button("upload file to cloudinary"):
+                uploaded_dict_obj = cloudinary.uploader.upload(choosedFile, resource_type="auto")
+                url = uploaded_dict_obj["secure_url"]
+                st.write(url)
+                st.write("file uploaded to cloudinary")
+
     elif opt == "Logout":
-        st.session_state.user=None
+        st.session_state.user = None
         st.success("logout successfully...")
         st.rerun()
 
@@ -48,44 +49,38 @@ def login_function():
     st.header("Login")
     with st.form("Login_Form"):
         email = st.text_input("Email")
-        password = st.text_input("Password",type="password")
-        btn=st.form_submit_button("Login")
+        password = st.text_input("Password", type="password")
+        btn = st.form_submit_button("Login")
 
-        if btn :
-            query="select * from users3 where email=%s and password = %s"
-            values=(email,password)
-            cursor_obj.execute(query,values)
-            loggedin_user=cursor_obj.fetchone()
+        if btn:
+            query = "select * from users3 where email=%s and password=%s"
+            values = (email, password)
+            cursor_obj.execute(query, values)
+            loggedin_user = cursor_obj.fetchone()
             st.session_state.user = loggedin_user
-            st.write("loggedin succesfully")
+            st.write("logged in successfully")
             st.rerun()
 
 def signup_function():
     st.header("SignUp")
-
     with st.form("SignUp_Form"):
-
         name = st.text_input("Name")
         email = st.text_input("Email")
         password = st.text_input("Password", type="password")
-
         btn = st.form_submit_button("SignUp")
+
         if btn:
-            query="insert into users3(name,email,password) values(%s,%s,%s)"
-            values=(name,email,password)
-            cursor_obj.execute(query,values)
+            query = "insert into users3(name,email,password) values(%s,%s,%s)"
+            values = (name, email, password)
+            cursor_obj.execute(query, values)
             conn_obj.commit()
-            st.write("user added successfully ")
+            st.write("user added successfully")
 
-
-if st.session_state.user == None:
-    login,signup = st.tabs(
-    ["Login","SignUp"]
-    )
+if st.session_state.user is None:
+    login, signup = st.tabs(["Login", "SignUp"])
     with signup:
         signup_function()
-
     with login:
-        login_function()    
+        login_function()
 else:
     dashboard()
